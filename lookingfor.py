e0fb9577c51bce7ca209a36ca114659af928df6a
XZDXRZ <13918165843@163.com>
2021-04-06 10:58:16 +0800

    更新

--- lookingfor.py
+++ lookingfor.py
@@ -14,18 +14,21 @@ for paraurl in href:
     bookurl = str(paraurl)[13:26]
     url.append(bookurl)
 
+f = open('./寻找走丢的舰娘/全文/寻找走丢的舰娘.txt','w',encoding = 'utf-8')
 rooturl =  'http://www.shuquge.com/txt/73804/'
 for i in url:
     n_url = rooturl + i
-    #print(n_url)
     page = rq.get(n_url)
     page.encoding = 'utf-8'
     soup = BeautifulSoup(page.text, 'lxml')
     title = soup.find('h1').text
-    f = open('./寻找走丢的舰娘/'+title+".txt",'w',encoding = 'utf-8')
+	#f = open('./寻找走丢的舰娘/'+title+".txt",'w',encoding = 'utf-8')
     f.write(title+'\n')
     article = soup.find('div', class_ = 'showtxt').text
     print(title,end = ' ')
     f.write(article)
-    f.close()
+    f.write('\n\n')
+    #f.close()
     print('完成')
+	
+f.close()
